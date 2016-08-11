c6f2799897899f3dae52a33092ec4d8f312b9974
Sola Li <tscc01@163.com>
2016-08-11 15:59:20 +0800

    update

---
+++ STUtilities/STUtilities/BasicUtility/BasicUtility.h
@@ -0,0 +1,14 @@
+//
+//  BasicUtility.h
+//  STUtilities
+//
+//  Created by 李晓春 on 16/8/11.
+//  Copyright © 2016年 tscc-sola. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <NSDate-Extensions/NSDate-Utilities.h>
+
+@interface BasicUtility : NSObject
+
+@end
---
+++ STUtilities/STUtilities/BasicUtility/BasicUtility.m
@@ -0,0 +1,13 @@
+//
+//  BasicUtility.m
+//  STUtilities
+//
+//  Created by 李晓春 on 16/8/11.
+//  Copyright © 2016年 tscc-sola. All rights reserved.
+//
+
+#import "BasicUtility.h"
+
+@implementation BasicUtility
+
+@end
